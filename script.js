72cb186424f9b3631f599af13af92212533bfdbf
murali2904-29 <chatg7478@gmail.com>
2024-10-08 23:53:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('login-form').addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevent default form submission
+    window.location.href = "dashboard.html"; // Redirect to dashboard
+});
+
+document.getElementById('signup-form').addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevent default form submission
+    window.location.href = "dashboard.html"; // Redirect to dashboard
+});
